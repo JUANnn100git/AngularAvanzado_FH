8708eaf7fe1ce329121e355d8109a2ab5f2ae8cc
JUANnn100 <jvavillanuevaaguinaga@gmail.com>
2019-06-03 17:54:09 -0500

    32. Resolución de tarea práctica #3

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
 // Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -17,23 +20,27 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    PagesComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent
+
+    //PagesComponent,
+    //DashboardComponent,
+    //ProgressComponent,
+    //Graficas1Component,
+
+    //HeaderComponent,
+    //SidebarComponent,
+    //BreadcrumbsComponent,
+    //NopagefoundComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
